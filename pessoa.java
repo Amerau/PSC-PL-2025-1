afa15b506378395fcff358cb719edc38bcfd168d
Amerau <guilhermeads91@gmail.com>
2025-04-04 12:26:10 -0300

    Exercícios de classe e pessoa executados em sala de aula

---
+++ pessoa.java
@@ -0,0 +1,15 @@
+class pessoa {
+    private double peso;
+    private double altura;
+    private int idade;
+    public String nome;
+    
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public double getPeso(){
+        return this.peso;
+    }
+
+}
